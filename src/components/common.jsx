f2475516bd2986bda8a160b575e89722dd52c2fe
byungjoo0806 <andybyungjoopark@gmail.com>
2024-04-08 22:27:10 +0900

    refresh

--- src/components/common.jsx
+++
@@ -1,6 +0,0 @@
-export const Wrapper = ({ children }) => <div className="pt-[16px] pb-[16px] md:pt-[24px] md:py-[16px]">{children}</div>
-export const Hero = ({ children }) => (
-    <h1 className="text-slate-900 font-extrabold text-[32px] md:text-[40px] md:pt-[5px]">{children}</h1>
-)
-export const Heading = ({ children }) => <h2 className="text-[24px] py-[24px] font-bold md:text-[40px]">{children}</h2>
-export const Typography = ({ children }) => <p className="text-[16px]">{children}</p>
