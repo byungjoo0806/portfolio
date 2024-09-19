b69af38bd0dcff8668a0e9c89d4847ab41d23d8f
byungjoo0806 <andybyungjoopark@gmail.com>
2024-09-19 11:16:46 -0700

    its been a while

--- src/contents/main-info/Projects.tsx
+++ src/contents/main-info/Projects.tsx
@@ -15,8 +15,8 @@ const Projects = () => {
 
     return (
         <Wrapper>
-            
-            <br/>
+
+            <br />
             <ul className="flex flex-col w-full">
                 {/* 현재 진행중 프로젝트 */}
                 <li className="flex flex-col gap-[32px] pb-5 border-b border-gray-400 md:flex-row">
@@ -27,9 +27,9 @@ const Projects = () => {
                             alt="crayon nft"
                         />
                         <ul className="flex text-[12px] text-slate-600 py-[8px] gap-[8px] flex-col items-center md:items-start">
-                            <li className="text-[16px]">Personal Project</li>
+                            <li className="text-[16px]">Startup Project</li>
                             <li>frontend</li>
-                            <li>blockchain</li>
+                            <li>backend</li>
                             <li>
                                 <button
                                     onClick={toggleModal4}
@@ -42,39 +42,27 @@ const Projects = () => {
                     </div>
                     <div className="w-full">
                         <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[24px] font-semibold pb-[16px]">
-                            CRAYON - Create Your Own NFT {' '}<br/> (in progress) <br/>
-                            {/* <a href="">
-                                <FaExternalLinkAlt />
-                            </a> */}
+                            Human Arc {' '}<br /> (in progress) <br />
                         </h2>
 
                         <p className="leading-5 text-slate-950 text-[12px] md:text-[16px] md:leading-7">
-                            Create your own NFTs and share your artistic senses and feelings with the whole world.
-                            Sell your own NFTs and buy others' to help grow the community.
-                            You can add image files or draw that you want to add to your NFTs.
-                            You can share your memories and enjoy others' experience as well.
+                            *** UNABLE TO REVEAL THE PROJECT INFO YET ***
                         </p>
-                        <br/>
+                        <br />
                         <h3 className="py-[16px] text-[16px] font-bold">Tech Stack</h3>
                         <div className="flex flex-row text-[32px] gap-[8px] py-[8px] px-[10px]">
                             <DiReact />
-                            <SiTypescript />
+                            <SiJavascript />
                             <SiTailwindcss />
                         </div>
 
                         <h3 className="py-[16px] text-[16px] font-bold">Roles and Responsibilities</h3>
                         <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
                             <li>
-                                Created a webpage using React with Typescript. No backend servers.
+                                Created an app with React-Native in Javscript
                             </li>
                             <li>
-                                Used Solidity and Web3.js for minting, buying, and selling NFTs.
-                            </li>
-                            <li>
-                                Used Remix for deploying contracts.
-                            </li>
-                            <li>
-                                Using Canvas, users would make their own images that would be added to NFTs. (coming soon)
+                                Used Firebase as the backend server and database
                             </li>
                         </ul>
                     </div>
@@ -106,7 +94,7 @@ const Projects = () => {
                     </div>
                     <div className="w-full">
                         <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[24px] font-semibold pb-[16px]">
-                            Real Estate STO <br/> (collaboration with a company){' '}
+                            Real Estate STO <br /> (collaboration with a company){' '}
                             <a href="https://bouncesto.site">
                                 <FaExternalLinkAlt />
                             </a>
@@ -117,7 +105,7 @@ const Projects = () => {
                             where users can buy subscriptions, trade tokens, and cast votes.
                             I was in charge of frontend development using React with Typescript and Tailwind CSS and voting contract using Solidity.
                         </p>
-                        <br/>
+                        <br />
                         <h3 className="py-[16px] text-[16px] font-bold">Tech Stack</h3>
                         <div className="flex flex-row text-[32px] gap-[8px] py-[8px] px-[10px]">
                             <DiReact />
@@ -134,7 +122,7 @@ const Projects = () => {
                         <h3 className="py-[16px] text-[16px] font-bold">My Roles & Responsibilities</h3>
                         <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
                             <li>
-                                built property list page, property detail page, trade page, mypage, vote list page, vote detail page 
+                                built property list page, property detail page, trade page, mypage, vote list page, vote detail page
                                 using React with Typescript and Tailwind CSS
                             </li>
                             <li>
@@ -185,7 +173,7 @@ const Projects = () => {
                             Steam 100 is a game recommendation website using Steam APIs.
                             I was in charge of fetching data using the APIs and rendering them with React.
                         </p>
-                        <br/>
+                        <br />
                         <h3 className="py-[16px] text-[16px] font-bold">Tech Stack</h3>
                         <div className="flex flex-row text-[32px] gap-[8px] py-[8px] px-[10px]">
                             <DiReact />
@@ -197,7 +185,7 @@ const Projects = () => {
                         <h3 className="py-[16px] text-[16px] font-bold"> My Roles & Properties</h3>
                         <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
                             <li>
-                                used three of Steam's API to fetch data from Steam database : 
+                                used three of Steam's API to fetch data from Steam database :
                                 GetMostPlayedGames, GetAppList, and appdeatils
                             </li>
                             <li>
@@ -246,7 +234,7 @@ const Projects = () => {
                             I was in charge of building mypage and profile edit page with HTML and CSS,
                             and deploying the project through AWS EC2.
                         </p>
-                        <br/>
+                        <br />
                         <h3 className="py-[16px] text-[16px] font-bold">Tech Stack</h3>
                         <div className="flex flex-row text-[32px] gap-[8px] py-[8px] px-[10px]">
                             <TiHtml5 />
@@ -282,51 +270,29 @@ const Projects = () => {
                             alt="crayon nft"
                         />
                         <ul className="flex text-[12px] text-slate-600 py-[8px] gap-[8px] flex-col items-center md:items-start">
-                            <li className="text-[16px]">Personal Project</li>
+                            <li className="text-[16px]">Startup Project</li>
                             <li>frontend</li>
-                            <li>blockchain</li>
-                            {/* <li>
-                                <button
-                                    onClick={toggleModal4}
-                                    className="px-[16px] py-[8px] bg-gray-800 rounded-[8px] text-white hover:bg-gray-950"
-                                >
-                                    Details
-                                </button>
-                            </li> */}
+                            <li>backend</li>
                         </ul>
                     </div>
                     <div className="w-full">
                         <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[20px] font-semibold pb-[16px]">
-                            CRAYON - Create Your Own NFT{' '}
+                            Human Arc{' '}
                         </h2>
 
                         <ul className="leading-5 list-disc pl-[20px] text-slate-950 text-[12px] md:text-[14px] md:leading-7">
-                            <li>a website where users can mint, buy, and sell NFTs</li>
-                            <li>users can create their own images for their own NFTs</li>
-                            <li>users can leave their favorite pictures or moments as NFTs that would never be lost</li>
-                            <li>users can get second-hand experience from others</li>
+                            <li>*** UNABLE TO REVEAL THE PROJECT INFO YET ***</li>
                         </ul>
 
                         <h3 className="pt-[16px] pb-[5px] text-[16px] font-bold">Tech Stack</h3>
                         <ul className="flex flex-col list-disc text-[14px] gap-[8px] py-[8px] px-[20px]">
                             <li>
-                                frontend : React, Typescript
+                                frontend : React-Native, Javascript
                             </li>
                             <li>
-                                blockchain : Solidity, Web3.js
+                                backend : Javascript (Firebase)
                             </li>
                         </ul>
-
-                        <h3 className="py-[16px] text-[16px] font-bold">Future Goals</h3>
-                        <div className="leading-8 text-[12px] md:text-[14px]">
-                            I would really want to have some game clients running in the page so that I can use those images as NFTs.
-                            For example, it would be wonderful if I could get a screenshot from Maplestory and turn that into the image of an NFT.
-                            Or it could be a League of Legends' champion from Summoner's Rift.
-                            There are just so many opportunities and possibilities that I would love to apply.
-                            The beginning of that is simple Canvas drawing action where users can draw or even doodle their own NFT images.
-                            Also, I would want to use Rust instead of Solidity. I've studied Rust a bit and I don't know when it will happen,
-                            but I would really love to apply Rust for blockchain.
-                        </div>
                     </div>
                 </div>
             </Modal>
@@ -344,14 +310,6 @@ const Projects = () => {
                             <li className="text-stone-500">2023. 11. 01 ~ 2023. 12. 05</li>
                             <li>frontend</li>
                             <li>blockchain</li>
-                            {/* <li>
-                                <button
-                                    onClick={toggleModal}
-                                    className="px-[16px] py-[8px] bg-gray-800 rounded-[8px] text-white hover:bg-gray-950"
-                                >
-                                    Details
-                                </button>
-                            </li> */}
                         </ul>
                     </div>
                     <div className="w-full">
@@ -388,13 +346,13 @@ const Projects = () => {
 
                         <h3 className="py-[16px] text-[16px] font-bold">Future Goals</h3>
                         <div className="leading-8 text-[12px] md:text-[14px]">
-                            For DAO system, I believe ERC4337 is the best-fit because of its paymaster API, 
-                            which allows a third-party to pay the gas fees for all the transactions 
-                            that occur when users try to cast their votes. 
-                            It really doesn’t make sense to pay your own money to express your thoughts. 
-                            I don’t think anyone would want to vote with the gas fees even if the gas fees are low. 
-                            I found out about ERC4337 a bit too late to apply it to our website, 
-                            so I would love to study more deeply on the paymaster and account abstraction 
+                            For DAO system, I believe ERC4337 is the best-fit because of its paymaster API,
+                            which allows a third-party to pay the gas fees for all the transactions
+                            that occur when users try to cast their votes.
+                            It really doesn’t make sense to pay your own money to express your thoughts.
+                            I don’t think anyone would want to vote with the gas fees even if the gas fees are low.
+                            I found out about ERC4337 a bit too late to apply it to our website,
+                            so I would love to study more deeply on the paymaster and account abstraction
                             that will make the STO community much more convenient.
                         </div>
                     </div>
@@ -415,14 +373,6 @@ const Projects = () => {
                             <li>frontend</li>
                             <li>backend</li>
                             <li>deploy</li>
-                            {/* <li>
-                                <button
-                                    onClick={toggleModal2}
-                                    className="px-[16px] py-[8px] bg-gray-800 rounded-[8px] text-white hover:bg-gray-950"
-                                >
-                                    Details
-                                </button>
-                            </li> */}
                         </ul>
                     </div>
                     <div className="w-full">
@@ -459,12 +409,12 @@ const Projects = () => {
 
                         <h3 className="py-[16px] text-[16px] font-bold">Future Goals</h3>
                         <div className="leading-8 text-[12px] md:text-[14px]">
-                            After using such APIs, I really wanted to try cloning the actual Steam website. 
-                            Steam250 website is only consisted of ranking lists, while actual Steam website has main page, 
-                            game detail page, store page, community page, and more. 
-                            Obviously, actual Steam page requires more work and maybe more complicated code. 
-                            It was my first time applying an API provided by a third party to receive data 
-                            which worked out pretty well in my opinion. So, I would love to create a more complicated 
+                            After using such APIs, I really wanted to try cloning the actual Steam website.
+                            Steam250 website is only consisted of ranking lists, while actual Steam website has main page,
+                            game detail page, store page, community page, and more.
+                            Obviously, actual Steam page requires more work and maybe more complicated code.
+                            It was my first time applying an API provided by a third party to receive data
+                            which worked out pretty well in my opinion. So, I would love to create a more complicated
                             but good-looking website on my own.
                         </div>
                     </div>
@@ -485,14 +435,6 @@ const Projects = () => {
                             <li>frontend</li>
                             <li>backend</li>
                             <li>deploy</li>
-                            {/* <li>
-                                <button
-                                    onClick={toggleModal3}
-                                    className="px-[16px] py-[8px] bg-gray-800 rounded-[8px] text-white hover:bg-gray-950"
-                                >
-                                    Details
-                                </button>
-                            </li> */}
                         </ul>
                     </div>
                     <div className="w-full">
@@ -524,13 +466,13 @@ const Projects = () => {
                         <h3 className="py-[16px] text-[16px] font-bold">Future Goals</h3>
                         <div className="leading-8 text-[12px] md:text-[14px]">
                             Using a smart wallet, I want to create a community where users can buy and sell NFTs
-                            by uploading posts. A post will have a single or multiple NFTs on sale and 
-                            users can buy such NFTs directly from the posts. 
+                            by uploading posts. A post will have a single or multiple NFTs on sale and
+                            users can buy such NFTs directly from the posts.
                             Users will also be able to comment and reply to share their thoughts on just about anything.
-                            Even if the blockchain network is getting larger, 
-                            it still has yet to become a widely-known technology 
-                            that not many countries have passed laws regarding blockchain. 
-                            Through the SNS-like NFT community, more and more users can have higher accessibility 
+                            Even if the blockchain network is getting larger,
+                            it still has yet to become a widely-known technology
+                            that not many countries have passed laws regarding blockchain.
+                            Through the SNS-like NFT community, more and more users can have higher accessibility
                             which will eventually increase the interest of more non-users that would lead to much larger NFT community.
                         </div>
                     </div>
